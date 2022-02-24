84925721e70047fa2745ca2fa0580b0f9e2be6c2
NosovMihail <32123595+NosovMihail@users.noreply.github.com>
2022-02-24 23:57:29 +0300

    Update dict.asm

--- lab2mine/dict.asm
+++ lab2mine/dict.asm
@@ -1,27 +1,23 @@
-
-extern string_equals
 global find_word
-
+extern string_equals
 section .text
 find_word:
 	mov r8, rdi
 	.loop:
-		mov r9,rsi
+		mov r9, rsi
 		test rsi, rsi
 		je .exit_not_found
-
-        mov rdi, r8
-		add rsi,8
+		add rsi, 8
+        	mov rdi, r8
 		call string_equals
 		mov rsi, r9
 		test rax, rax
 		jnz .exit_found
-
-		mov rsi,[rsi]
+		mov rsi, [rsi]
 		jmp .loop
 	.exit_found:
 		mov rax, rsi
 		ret
 	.exit_not_found:
-		mov rax,0
+		mov rax, 0
 		ret
